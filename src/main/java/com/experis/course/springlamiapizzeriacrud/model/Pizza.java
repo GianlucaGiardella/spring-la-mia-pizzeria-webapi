0bfb0369ea6efacb2e66ffdf87887813a59bf994
GianluGiarde <gianlugiarde@gmail.com>
2023-11-09 16:57:39 +0100

    Initial commit

---
+++ src/main/java/com/experis/course/springlamiapizzeriacrud/model/Pizza.java
@@ -0,0 +1,67 @@
+package com.experis.course.springlamiapizzeriacrud.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "pizzas")
+public class Pizza {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    private String image_url;
+    private BigDecimal price;
+    private LocalDateTime created_at;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+}
---
+++ src/main/java/com/experis/course/springlamiapizzeriacrud/repository/PizzaRepository.java
@@ -0,0 +1,8 @@
+package com.experis.course.springlamiapizzeriacrud.repository;
+
+import com.experis.course.springlamiapizzeriacrud.model.Pizza;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
+
+}
