6983faeaf4b1fc3d84ed52a3e82c7882c4d2fe2f
GianluGiarde <gianlugiarde@gmail.com>
2023-11-13 17:31:12 +0100

    add validations

--- src/main/java/com/experis/course/springlamiapizzeriacrud/controller/PizzaController.java
+++ src/main/java/com/experis/course/springlamiapizzeriacrud/controller/PizzaController.java
@@ -2,14 +2,14 @@ package com.experis.course.springlamiapizzeriacrud.controller;
 
 import com.experis.course.springlamiapizzeriacrud.model.Pizza;
 import com.experis.course.springlamiapizzeriacrud.repository.PizzaRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -27,7 +27,7 @@ public class PizzaController {
 
         List<Pizza> pizzaList;
 
-        if(search.isPresent()) {
+        if (search.isPresent()) {
             pizzaList = pizzaRepository.findByNameContainingIgnoreCase(search.get());
         } else {
             pizzaList = pizzaRepository.findAll();
@@ -43,12 +43,38 @@ public class PizzaController {
 
         Optional<Pizza> result = pizzaRepository.findById(id);
 
-        if(result.isPresent()) {
+        if (result.isPresent()) {
             model.addAttribute("pizza", result.get());
 
             return "pizzas/show";
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pizza with id " + id + " not found!");
         }
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("pizza", new Pizza());
+        return "pizzas/create";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "pizzas/create";
+        }
+
+        Pizza newPizza = null;
+        try {
+            newPizza = pizzaRepository.save(formPizza);
+        } catch (RuntimeException e) {
+            bindingResult.addError(new FieldError("book", "isbn", formPizza.getPrice(), false, null, null,
+                    "Price must be greaten than 0"));
+
+            return "pizzas/create";
+        }
+
+        return "redirect:/pizzas/show/" + newPizza.getId();
     }
 }
